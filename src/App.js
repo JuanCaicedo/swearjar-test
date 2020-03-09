7370ebbce220436e32de1c7130e6da68a3a9e5dd
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-03-09 15:12:56 -0700

    add swearjar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import swearjar from "swearjar-extended";
 
 function App() {
   return (
